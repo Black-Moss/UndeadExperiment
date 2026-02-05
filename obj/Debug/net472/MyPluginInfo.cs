fd0ade887633cec429a2e157b0c2b0219c6c3341
Black_Moss <kytheminecraft@outlook.com>
2026-02-06 05:32:37 +0800

    UndeadExperiment

--- obj/Debug/net472/MyPluginInfo.cs
+++ obj/Debug/net472/MyPluginInfo.cs
@@ -1,9 +1,9 @@
-namespace BlackMossTemplate
+namespace UndeadExperiment
 {
     public static class MyPluginInfo
     {
-        public const string PLUGIN_GUID = "BlackMossTemplate";
-        public const string PLUGIN_NAME = "BlackMossTemplate";
+        public const string PLUGIN_GUID = "UndeadExperiment";
+        public const string PLUGIN_NAME = "UndeadExperiment";
         public const string PLUGIN_VERSION = "1.0.0";
     }
 }
