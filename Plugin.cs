bc73fa3cdfe0c632248f144d080206f8ca86c335
Black_Moss <kytheminecraft@outlook.com>
2026-02-06 16:33:46 +0800

    血粘

--- Plugin.cs
+++ Plugin.cs
@@ -60,61 +60,61 @@ public class Plugin : BaseUnityPlugin
     
     private static void Heal()
     {
-        Logger.LogInfo("HEAL!");
-        foreach (Limb limb in PlayerCamera.main.body.limbs) {
-            limb.muscleHealth = 100f;
-            limb.skinHealth = 100f;
-            limb.boneHealTimer = 0.0f;
-            limb.dislocationTimer = 0.0f;
-            limb.infectionAmount = 0.0f;
-            limb.bleedAmount = 0.0f;
-            limb.pain = 0.0f;
-            limb.shrapnel = 0;
-            limb.infected = false;
-        }
-        PlayerCamera.main.body.brainHealth = 100f;
-        PlayerCamera.main.body.bloodAmount = 100f;
-        PlayerCamera.main.body.oxygenAmount = 100f;
-        PlayerCamera.main.body.hunger = 100f;
-        PlayerCamera.main.body.thirst = 100f;
-        PlayerCamera.main.body.septicShock = 0.0f;
-        PlayerCamera.main.body.temperature = 37f;
-        PlayerCamera.main.body.sicknessAmount = 0.0f;
-        PlayerCamera.main.body.consciousness = 100f;
-        PlayerCamera.main.body.stamina = 100f;
-        PlayerCamera.main.body.energy = 100f;
-        PlayerCamera.main.body.happiness = 0.0f;
-        PlayerCamera.main.body.weightOffset = 0.0f;
-        PlayerCamera.main.body.radiationSickness = 0.0f;
-        PlayerCamera.main.body.disfigured = false;
-        PlayerCamera.main.body.eyeGone = false;
-        PlayerCamera.main.body.internalBleeding = 0.0f;
-        PlayerCamera.main.body.hemothorax = 0.0f;
-        PlayerCamera.main.body.traumaAmount = 0.0f;
-        PlayerCamera.main.body.dirtyness = 0.0f;
-        PlayerCamera.main.body.wetness = 0.0f;
-        PlayerCamera.main.body.badSleepAmount = 0.0f;
-        PlayerCamera.main.body.hearingLoss = 0.0f;
-        PlayerCamera.main.body.antidepressantHappiness = 0.0f;
-        PlayerCamera.main.body.opiateHappiness = 0.0f;
-        PlayerCamera.main.body.antibioticImmunityTime = 0.0f;
-        PlayerCamera.main.body.brainGrowSickness = 0.0f;
-        PlayerCamera.main.body.triedRollingLastStand = false;
-        PlayerCamera.main.body.succesfullyRolledLastStand = false;
-        PlayerCamera.main.body.lastStandTime = -1000f;
-        PlayerCamera.main.body.adrenaline = 0.0f;
-        PlayerCamera.main.body.curAdrenaline = 0.0f;
-        Painkillers component1;
-        if (PlayerCamera.main.body.TryGetComponent<Painkillers>(out component1))
-            UnityEngine.Object.Destroy((UnityEngine.Object) component1);
-        SleepingPills component2;
-        if (PlayerCamera.main.body.TryGetComponent<SleepingPills>(out component2))
-            UnityEngine.Object.Destroy((UnityEngine.Object) component2);
-        Antidepressants component3;
-        if (PlayerCamera.main.body.TryGetComponent<Antidepressants>(out component3))
-            UnityEngine.Object.Destroy((UnityEngine.Object) component3);
-        BleachDrink component4;
-        if (PlayerCamera.main.body.TryGetComponent<BleachDrink>(out component4))
-            UnityEngine.Object.Destroy((UnityEngine.Object) component4);
+    foreach (Limb limb in PlayerCamera.main.body.limbs) {
+        limb.muscleHealth = 100f;
+        limb.skinHealth = 100f;
+        limb.boneHealTimer = 0.0f;
+        limb.dislocationTimer = 0.0f;
+        limb.infectionAmount = 0.0f;
+        limb.bleedAmount = 0.0f;
+        limb.pain = 0.0f;
+        limb.shrapnel = 0;
+        limb.infected = false;
+    }
+    PlayerCamera.main.body.brainHealth = 100f;
+    PlayerCamera.main.body.bloodAmount = 100f;
+    PlayerCamera.main.body.bloodViscous = 0;
+    PlayerCamera.main.body.oxygenAmount = 100f;
+    PlayerCamera.main.body.hunger = 100f;
+    PlayerCamera.main.body.thirst = 100f;
+    PlayerCamera.main.body.temperature = 37f;
+    PlayerCamera.main.body.consciousness = 100f;
+    PlayerCamera.main.body.stamina = 100f;
+    PlayerCamera.main.body.energy = 100f;
+    PlayerCamera.main.body.sicknessAmount = 0.0f;
+    PlayerCamera.main.body.septicShock = 0.0f;
+    PlayerCamera.main.body.radiationSickness = 0.0f;
+    PlayerCamera.main.body.traumaAmount = 0.0f;
+    PlayerCamera.main.body.internalBleeding = 0.0f;
+    PlayerCamera.main.body.hemothorax = 0.0f;
+    PlayerCamera.main.body.dirtyness = 0.0f;
+    PlayerCamera.main.body.wetness = 0.0f;
+    PlayerCamera.main.body.happiness = 0.0f;
+    PlayerCamera.main.body.antidepressantHappiness = 0.0f;
+    PlayerCamera.main.body.opiateHappiness = 0.0f;
+    PlayerCamera.main.body.hearingLoss = 0.0f;
+    PlayerCamera.main.body.weightOffset = 0.0f;
+    PlayerCamera.main.body.badSleepAmount = 0.0f;
+    PlayerCamera.main.body.adrenaline = 0.0f;
+    PlayerCamera.main.body.curAdrenaline = 0.0f;
+    PlayerCamera.main.body.antibioticImmunityTime = 0.0f;
+    PlayerCamera.main.body.brainGrowSickness = 0.0f;
+    PlayerCamera.main.body.disfigured = false;
+    PlayerCamera.main.body.eyeGone = false;
+    PlayerCamera.main.body.triedRollingLastStand = false;
+    PlayerCamera.main.body.succesfullyRolledLastStand = false;
+    PlayerCamera.main.body.lastStandTime = -1000f;
+    Painkillers component1;
+    if (PlayerCamera.main.body.TryGetComponent<Painkillers>(out component1))
+        UnityEngine.Object.Destroy((UnityEngine.Object) component1);
+    SleepingPills component2;
+    if (PlayerCamera.main.body.TryGetComponent<SleepingPills>(out component2))
+        UnityEngine.Object.Destroy((UnityEngine.Object) component2);
+    Antidepressants component3;
+    if (PlayerCamera.main.body.TryGetComponent<Antidepressants>(out component3))
+        UnityEngine.Object.Destroy((UnityEngine.Object) component3);
+    BleachDrink component4;
+    if (PlayerCamera.main.body.TryGetComponent<BleachDrink>(out component4))
+        UnityEngine.Object.Destroy((UnityEngine.Object) component4);
     }
 }
